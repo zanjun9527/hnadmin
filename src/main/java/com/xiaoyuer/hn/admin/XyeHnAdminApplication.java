2b214636d1eaa1d27ace7e7db8b7405b67d69c4f
zhanjun <179896578@qq.com>
2020-08-03 13:57:33 +0800

    首次提交

---
+++ src/main/java/com/xiaoyuer/hn/admin/XyeHnAdminApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyuer.hn.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyeHnAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyeHnAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoyuer/hn/admin/XyeHnAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyuer.hn.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyeHnAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
